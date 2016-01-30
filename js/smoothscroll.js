cfaa054307074f2a8b0bb4773c1a69dca77f79f2
Hao Zhu <haozhu@hsl.harvard.edu>
2016-01-30 11:48:30 -0500

    add smoothscroll to intruction page

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -7,28 +7,9 @@ $(function() {
 			if (target.length) {
 				$('html,body').animate({
 					scrollTop: target.offset().top
-				}, 1000);
+				}, 800);
 				return false;
 			}
 		}
 	});
-});
-
-$(".navbar-nav li a[href^='#']").on('click', function(e) {
-	// prevent default anchor click behavior
-	e.preventDefault();
-
-	// store hash
-	var hash = this.hash;
-
-	// animate
-	$('html, body').animate({
-		scrollTop: $(this.hash).offset().top
-	}, 300, function(){
-
-		// when done, add hash to url
-		// (default click behaviour)
-		window.location.hash = hash;
-	});
-
 });
